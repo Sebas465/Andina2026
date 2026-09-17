7aa612f4281cf27024c1436a3f96d360977e5254
SunsetInFall <samirarlikchavezhernandez@gmail.com>
2026-09-17 14:49:15 -0500

    primera version

---
+++ src/main/java/org/example/andina2026/Andina2026Application.java
@@ -0,0 +1,13 @@
+package org.example.andina2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Andina2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Andina2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/andina2026/Andina2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.andina2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Andina2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
