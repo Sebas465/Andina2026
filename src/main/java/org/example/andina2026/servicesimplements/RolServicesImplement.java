c21a3d3bfd6b7f7b6b2cb06c78db45b0f0f8ef0f
Sebastian_Taipe <u20211c755@upc.edu.pe>
2026-09-19 03:34:46 -0500

    version3

--- src/main/java/org/example/andina2026/servicesimplements/RolServicesImplement.java
+++ src/main/java/org/example/andina2026/servicesimplements/RolServicesImplement.java
@@ -1,25 +1,26 @@
 package org.example.andina2026.servicesimplements;
 
 import org.example.andina2026.entities.Rol;
+import org.example.andina2026.repositories.IRolRepository;
 import org.example.andina2026.servicesinterfaces.IRolService;
 
 import java.util.List;
 
 public class RolServicesImplement implements IRolService {
 
-    public final IRolService rS;
+    public final IRolRepository rR;
 
-    public RolServicesImplement(IRolService rS) {
-        this.rS = rS;
+    public RolServicesImplement(IRolRepository rR) {
+        this.rR = rR;
     }
 
     @Override
     public void insert(Rol r) {
-
+        rR.save(r);
     }
 
     @Override
     public List<Rol> list() {
-        return List.of();
+        return rR.findAll();
     }
 }
