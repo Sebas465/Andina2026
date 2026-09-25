34256bbc1d9884088f4e4332a90615fee3d91256
Eddinson Huillca <huillcaeddinson@gmail.com>
2026-09-25 11:25:30 -0500

    feat: agrega gestión de materiales y asignación docente

--- src/main/java/org/example/andina2026/entities/AsignacionDocente.java
+++ src/main/java/org/example/andina2026/entities/AsignacionDocente.java
@@ -104,3 +104,4 @@ public class AsignacionDocente {
         this.colegio = colegio;
     }
 }
+//Commit
--- src/main/java/org/example/andina2026/entities/Material.java
+++ src/main/java/org/example/andina2026/entities/Material.java
@@ -89,3 +89,4 @@ public class Material {
         this.persona = persona;
     }
 }
+//Commit
